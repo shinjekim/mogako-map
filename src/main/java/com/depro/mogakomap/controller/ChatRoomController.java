2a4a4bf0e85974126d8a3936edf1698251409518
kimshinje <shinjehere@gmail.com>
2019-07-03 18:19:52 +0900

    배포를 위해 오류가 있는 service를 일단 삭제

--- src/main/java/com/depro/mogakomap/controller/ChatRoomController.java
+++ src/main/java/com/depro/mogakomap/controller/ChatRoomController.java
@@ -2,7 +2,6 @@ package com.depro.mogakomap.controller;
 
 import com.depro.mogakomap.dto.ChatRoomDto;
 import com.depro.mogakomap.repository.ChatRoomRepository;
-import com.depro.mogakomap.service.ChatRoomService;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,7 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 @AllArgsConstructor
 public class ChatRoomController {
     private ChatRoomRepository chatRoomRepository;
-    private ChatRoomService chatRoomService;
 
     @PostMapping("/chatroom/create")
     public void createChatRoom(@RequestBody ChatRoomDto dto){
