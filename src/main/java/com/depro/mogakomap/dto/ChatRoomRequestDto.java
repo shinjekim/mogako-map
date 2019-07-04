dd97faaf6eeb48a36e1504928f49ce98825cd9b4
kimshinje <shinjehere@gmail.com>
2019-07-04 12:35:59 +0900

    하나의 dto를 request/response dto로 나누어 변경

--- src/main/java/com/depro/mogakomap/dto/ChatRoomRequestDto.java
+++ src/main/java/com/depro/mogakomap/dto/ChatRoomRequestDto.java
@@ -1,16 +1,26 @@
 package com.depro.mogakomap.dto;
 
 import com.depro.mogakomap.entity.ChatRoom;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
-@Data
+/**
+ * Created by shinjekim on 2019.07.04
+ * shinjehere@gmail.com
+ */
+@Getter
+@Setter
 @NoArgsConstructor
-public class ChatRoomDto {
+public class ChatRoomRequestDto {
     private Long chatRoomId;
     private String chatRoomTitle;
     private String mapMarkerLocation;
+
+    @Builder
+    public ChatRoomRequestDto(Long chatRoomId, String chatRoomTitle, String mapMarkerLocation){
+        this.chatRoomId = chatRoomId;
+        this.chatRoomTitle = chatRoomTitle;
+        this.mapMarkerLocation = mapMarkerLocation;
+    }
 
     public ChatRoom toEntity(){
         return ChatRoom.builder()
---
+++ src/main/java/com/depro/mogakomap/dto/ChatRoomResponseDto.java
@@ -0,0 +1,21 @@
+package com.depro.mogakomap.dto;
+
+import com.depro.mogakomap.entity.ChatRoom;
+import lombok.Getter;
+
+/**
+ * Created by shinjekim on 2019.07.04
+ * shinjehere@gmail.com
+ */
+@Getter
+public class ChatRoomResponseDto {
+    private Long chatRoomId;
+    private String chatRoomTitle;
+    private String mapMarkerLocation;
+
+    public ChatRoomResponseDto(ChatRoom entity){
+        chatRoomId = entity.getChatRoomId();
+        chatRoomTitle = entity.getChatRoomTitle();
+        mapMarkerLocation = entity.getMapMarkerLocation();
+    }
+}
