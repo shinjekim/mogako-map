6ff82815bf42a983b82665495283a9ff2d45b17f
kimshinje <shinjehere@gmail.com>
2019-07-04 13:06:14 +0900

    변경된 dto 반영하여 소켓 메시징 완성

--- src/main/java/com/depro/mogakomap/controller/ChatController.java
+++ src/main/java/com/depro/mogakomap/controller/ChatController.java
@@ -1,13 +1,19 @@
 package com.depro.mogakomap.controller;
 
-import com.depro.mogakomap.dto.ChatDto;
+import com.depro.mogakomap.dto.ChatRequestDto;
+import com.depro.mogakomap.dto.ChatResponseDto;
+import com.depro.mogakomap.dto.ChatRoomResponseDto;
 import com.depro.mogakomap.entity.Chat;
+import com.depro.mogakomap.entity.ChatRoom;
 import com.depro.mogakomap.repository.ChatRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+import java.util.Optional;
 
 @RestController
 @AllArgsConstructor
@@ -27,9 +33,11 @@ public class ChatController {
     }
 
     @MessageMapping("/chatmessage")
-    public void setAndGetChatMessage(@PathVariable Long chatRoomId, Chat chatMessage){
-//        ChatDto chatDto = new ChatDto();
-//        chatDto.setChatMessage(chatMessage);
-        this.template.convertAndSend("/chatroom/"+chatRoomId, chatMessage);
+    public void setAndGetChatMessage(ChatRequestDto chatRequestDto){
+        chatRepository.save(chatRequestDto.toEntity());
+        Long chatRoomId = chatRequestDto.getChatRoom().getChatRoomId();
+        //TODO: Optional에 엔터티 타입을 넣는게 맞는건지?
+        Optional<Chat> chatResponseDto = chatRepository.findById(chatRoomId);
+        this.template.convertAndSend("/chatroom/"+chatRoomId, chatResponseDto);
     }
 }
