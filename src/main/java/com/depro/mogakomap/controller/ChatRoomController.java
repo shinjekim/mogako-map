33986b25eb823f7c3e76f4e220e2800ee2f7f31f
kimshinje <shinjehere@gmail.com>
2019-07-28 17:12:57 +0900

    주석 추가

--- src/main/java/com/depro/mogakomap/controller/ChatRoomController.java
+++ src/main/java/com/depro/mogakomap/controller/ChatRoomController.java
@@ -1,17 +1,12 @@
 package com.depro.mogakomap.controller;
 
-import com.depro.mogakomap.dto.ChatMessageResponseDto;
 import com.depro.mogakomap.dto.ChatRoomRequestDto;
 import com.depro.mogakomap.dto.ChatRoomResponseDto;
-import com.depro.mogakomap.repository.ChatMessageRepository;
 import com.depro.mogakomap.repository.ChatRoomRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 /**
  * Created by shinjekim on 2019.07.04
@@ -22,10 +17,16 @@ import java.util.List;
 public class ChatRoomController {
     private ChatRoomRepository chatRoomRepository;
 
+    /**
+     * 채팅방을 생성합니다.
+     * @param chatRoomRequestDto
+     * @return
+     */
     @PostMapping("/chatroom/create")
     public ChatRoomResponseDto createChatRoom(@RequestBody ChatRoomRequestDto chatRoomRequestDto){
         chatRoomRepository.save(chatRoomRequestDto.toEntity());
         ChatRoomResponseDto chatRoomResponseDto = new ChatRoomResponseDto(chatRoomRepository.findByChatRoomLocation(chatRoomRequestDto.getChatRoomLocation()));
+        //TODO: 개발 완료 후 삭제할것
         System.out.println("채팅방생성: "+chatRoomResponseDto.getChatRoomTitle());
         return chatRoomResponseDto;
     }
